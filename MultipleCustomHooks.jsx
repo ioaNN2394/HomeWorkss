7b28257aa6f4c344ffe1a0cfb3c93343d9c58045
Johan Quimbayo <114038390+ioaNN2394@users.noreply.github.com>
2024-10-09 11:29:12 -0500

    Add files via upload

---
+++ MultipleCustomHooks.jsx
@@ -0,0 +1,34 @@
+// /components/MultipleCustomHooks.jsx
+
+import React from 'react';
+import { useCounter } from '../hooks/useCounter';
+import { useFetch } from '../hooks/UseFetch';
+
+export const MultipleCustomHooks = () => {
+  const { counter, increment } = useCounter(1);
+  const { data, isLoading } = useFetch(`https://breakingbadapi.com/api/quotes/${counter}`);
+
+  return (
+    <>
+      <h1>MultipleCustomHooks</h1>
+      <hr />
+
+      {isLoading ? (
+        <div className="alert alert-info text-center">
+          Loading...
+        </div>
+      ) : (
+        <blockquote className="blockquote text-end">
+          <p className="mb-1"> {data[0]?.quote} </p>
+          <footer className="blockquote-footer"> {data[0]?.author} </footer>
+        </blockquote>
+      )}
+
+      <button className="btn btn-primary" onClick={() => increment()}>
+        Next Quote
+      </button>
+    </>
+  );
+};
+
+export default MultipleCustomHooks;
