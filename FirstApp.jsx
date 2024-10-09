5ffe022c61852b4dc37a93a3df5fbc9aebf6cd37
Johan Quimbayo <114038390+ioaNN2394@users.noreply.github.com>
2024-10-09 11:19:03 -0500

    Add files via upload

---
+++ FirstApp.jsx
@@ -0,0 +1,13 @@
+// FirstApp.js
+import React from 'react';
+
+const FirstApp = () => {
+  return (
+    <div>
+      <h1>New Title</h1>
+      <span>10</span>
+    </div>
+  );
+};
+
+export default FirstApp;
